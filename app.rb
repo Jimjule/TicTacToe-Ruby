c201fdc7d5b77637ed5de79df7c639e07bd7ccf4
Jimjule <roadknightj@gmail.com>
2020-01-29 14:56:39 +0000

    First commit - Returns 3x3 board

---
+++ app.rb
@@ -0,0 +1,4 @@
+require './lib/tic_tac_toe.rb'
+
+board = Board.new
+board.view_board
---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,9 @@
+class Board
+    def initialize
+        @board = [['-----'],['|   |'],['|   |'],['|   |'],['-----']]
+    end
+
+    def view_board
+        return @board
+    end
+end
