f48ff6160f1582460eb717ead889d155d42f97e1
Jimjule <roadknightj@gmail.com>
2020-02-04 12:08:52 +0000

    Added before each do hook to feature tests

--- spec/features/can_win_and_declare_winner_spec.rb
+++ spec/features/can_win_and_declare_winner_spec.rb
@@ -3,24 +3,25 @@ require 'player'
 # frozen_string_literal: true
 
 describe Player do
+  before(:each) do
+    @player = Player.new
+  end
+
   it 'Can win diagonally' do
-    player = Player.new
-    allow(player.board).to receive(:gets).and_return('9', '2', '5', '3', '1', '4', '6', '7', '8')
-    player.go
-    expect(player.winner).to be true
+    allow(@player.board).to receive(:gets).and_return('9', '2', '5', '3', '1', '4', '6', '7', '8')
+    @player.go
+    expect(@player.winner).to be true
   end
 
   it 'Can win a row' do
-    player = Player.new
-    allow(player.board).to receive(:gets).and_return('9', '1', '7', '2', '6', '3', '4', '5', '8')
+    allow(@player.board).to receive(:gets).and_return('9', '1', '7', '2', '6', '3', '4', '5', '8')
     player.go
-    expect(player.winner).to be true
-    expect { player.go }.to output('O is the winner!').to_stdout
+    expect(@player.winner).to be true
+    expect { @player.go }.to output('O is the winner!').to_stdout
   end
 
   it 'Can win a column' do
-    player = Player.new
-    allow(player.board).to receive(:gets).and_return('1', '2', '4', '5', '7', '8', '3', '6', '9')
-    expect { player.go }.to output('X is the winner!').to_stdout
+    allow(@player.board).to receive(:gets).and_return('1', '2', '4', '5', '7', '8', '3', '6', '9')
+    expect { @player.go }.to output('X is the winner!').to_stdout
   end
 end
