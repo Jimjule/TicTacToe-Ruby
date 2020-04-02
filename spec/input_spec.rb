119bdd57fa1b9299ce20ec655e87091faa9e1829
Jimjule <roadknightj@gmail.com>
2020-04-02 13:33:42 +0100

    Adds console tests

--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,11 +1,27 @@
 require 'consoleInOut'
 
 describe ConsoleInOut do
-  it 'Can mock input and output' do
+  it 'Writes to output' do
     input = "It's a string!"
     output = []
     consoleInOut = ConsoleInOut.new(input, output)
     consoleInOut.write(input)
     expect(consoleInOut.output[0]).to eq("It's a string!")
+  end
+
+  it 'Validates board size' do
+    input = ''
+    output = []
+    consoleInOut = ConsoleInOut.new(input, output)
+    consoleInOut.valid_board_size(13)
+    expect(consoleInOut).not_to receive(:set_board_size)
+  end
+
+  it 'Validates player move' do
+    input = ''
+    output = []
+    consoleInOut = ConsoleInOut.new(input, output)
+    consoleInOut.valid_player_input(5, 9)
+    expect(consoleInOut).not_to receive(:select_move)
   end
 end
