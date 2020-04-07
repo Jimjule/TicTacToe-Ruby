a6b0eaba444d373f78fb98446c361dbe018f68f0
Jimjule <roadknightj@gmail.com>
2020-04-07 09:28:27 +0100

    Amends a test with an assertion

--- spec/features/set_board_size_spec.rb
+++ spec/features/set_board_size_spec.rb
@@ -13,7 +13,8 @@ describe Board do
     expect(board.inOut.output.join).to eq("------|1234||5678||9101112||13141516|------")
   end
 
-  it 'Can make a 9x9 board' do
-    board = Board.new(@inOut, @validate, 6)
+  it 'A 9x9 board has 81 turns' do
+    board = Board.new(@inOut, @validate, 9)
+    expect(board.max_turns).to eq 81
   end
 end
