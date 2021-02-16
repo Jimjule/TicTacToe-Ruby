ff2879bbe692ab342a0ddb593a4d9d019cf7d337
Jimjule <roadknightj@gmail.com>
2021-02-16 16:27:57 +0000

    Adds tests for player factory

---
+++ spec/player_factory_spec.rb
@@ -0,0 +1,19 @@
+require 'player_factory'
+
+describe Player_factory do
+  let(:input) { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:validate) { Validate.new }
+
+  before(:each) do
+    @in_out = Console_in_out.new(input, output, validate)
+  end
+
+  it 'Returns a Human Player' do
+    expect(Player_factory.generate('X', false, @in_out, 'Test Player')).to be_kind_of(Human_player)
+  end
+
+  it 'Returns a Computer Player' do
+    expect(Player_factory.generate('O', true, @in_out, 'Test Computer')).to be_kind_of(Computer_player)
+  end
+end
