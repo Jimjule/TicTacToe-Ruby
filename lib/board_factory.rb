22bbec1eb904034b18889fbfb548f5b81e95809e
Jimjule <roadknightj@gmail.com>
2021-02-16 14:25:06 +0000

    Uses more consistent argument order in board factory

--- lib/board_factory.rb
+++ lib/board_factory.rb
@@ -8,11 +8,11 @@ class Board_factory
     in_out = in_out
     validate = validate
 
-    board_size = get_board_size(validate, in_out)
+    board_size = get_board_size(in_out, validate)
     @board = Board.new(validate, board_size)
   end
 
-  def get_board_size(validate, in_out)
+  def get_board_size(in_out, validate)
     board_size = ''
     until validate.is_valid_board_size?(board_size)
       board_size = in_out.set_board_size
