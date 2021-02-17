bf7268cc7960ad9847d3ce27beaab39d1f0f473a
Jimjule <roadknightj@gmail.com>
2021-02-17 11:55:28 +0000

    Uses appropriate class for output in one instance

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,15 +1,15 @@
 class Human_player
   attr_reader :id, :mark
   def initialize(id, mark, in_out)
-    @in_out = in_out
     @id = id
     @mark = mark
+    @in_out = in_out
   end
 
   def select_move(board)
     valid_move = false
     until (valid_move)
-      print("Please enter a free number from 1-#{board.max_turns}\n")
+      @in_out.print("Please enter a free number from 1-#{board.max_turns}\n")
       player_input = @in_out.select_move - 1
       valid_move = player_input <= board.max_turns && player_input >= -board.max_turns && board.is_square_free?(player_input)
     end
